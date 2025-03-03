4450855b2f1f37ed8a6fc2b45fa09dc1be614961
KrishaPanwala <145736673+KrishaPanwala@users.noreply.github.com>
2025-03-03 12:41:08 +0530

    Update Chatbot.py

--- Gemini_chatbot/Chatbot.py
+++ Gemini_chatbot/Chatbot.py
@@ -15,7 +15,7 @@ import speech_recognition as sr
 import numpy as np
 import queue
 
-GOOGLE_API_KEY = "AIzaSyCqKX3DNKoYl4d-6shJebCI5PlMhEG-fA0"  # Replace with your actual key
+GOOGLE_API_KEY = "GEMINI_API_KEY"  # Replace with your actual key
 genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel('gemini-1.5-pro-latest')
